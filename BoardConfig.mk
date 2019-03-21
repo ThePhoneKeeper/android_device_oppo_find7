126a0932cbba4f7852d47558b182cce4622dff56
Rishab Nair <rishab9706@gmail.com>
2019-03-21 05:38:48 +0000

    find7: Rebrand to aosp
    Signed-off-by: Rishab Nair <rishab9706@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-TARGET_KERNEL_CONFIG := lineageos_find7_defconfig
+TARGET_KERNEL_CONFIG := aosp_find7_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
 # Assert
