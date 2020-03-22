0c6574751568f138c63d493ef15e009b39125e14
Rishab Nair <rishab9706@gmail.com>
2020-03-22 13:15:44 +0000

    find7: Remove obselete nfc blobs

--- find7.mk
+++ find7.mk
@@ -58,11 +58,6 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
-
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
